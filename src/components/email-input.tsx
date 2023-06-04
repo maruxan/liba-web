56d769cae8cc322906e4c18ce97006aa9181f4bd
maruxan <garavagliamauricio@gmail.com>
2023-06-04 03:30:49 -0300

    feat: email input form

--- src/components/email-input.tsx
+++ src/components/email-input.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { FormEvent } from "react";
+import { CgArrowLongRight } from "react-icons/cg";
 
 const EmailInput = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("FORM SUBMIT :::: ", e);
+  };
+
   return (
-    <div className="join">
-      <input className="input join-item" placeholder="Email" />
-      <button className="btn join-item rounded-r-full">Subscribe</button>
-    </div>
+    <form className="flex flex-col" onSubmit={handleSubmit}>
+      <div className="join rounded-none border-2 border-white w-full md:w-fit">
+        <label
+          htmlFor="email"
+          className="label join-item uppercase px-8 bg-white text-black font-bold tracking-wide"
+        >
+          email
+        </label>
+        <input
+          name="email"
+          type="email"
+          className="input join-item w-full md:w-96"
+          placeholder="example@email.com"
+        />
+      </div>
+      <button type="submit" className="flex items-center self-end gap-3">
+        <div className="font-bold text-2xl text-[#F9D846] pb-2">
+          catch me up
+        </div>
+        <CgArrowLongRight className="w-14 h-14" color="#F9D846" />
+      </button>
+    </form>
   );
 };
 
