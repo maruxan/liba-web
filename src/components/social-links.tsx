3e04ef5d49c5c3136cccb8967bf46d10c0f798f8
maruxan <garavagliamauricio@gmail.com>
2023-06-19 21:20:59 -0300

    feat: flex social links

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -1,13 +1,31 @@
 import React from "react";
+import clsx from "clsx";
+import { Color, fontColorClasses } from "@/types";
 import { BsBehance, BsDribbble, BsInstagram } from "react-icons/bs";
 
-const SocialLinks = () => {
+type Props = {
+  direction?: "col" | "row";
+  color?: Color;
+};
+
+const SocialLinks = ({ direction = "col", color = "primary" }: Props) => {
   return (
-    <div className="flex flex-col gap-6 items-center justify-center">
+    <div
+      className={clsx(
+        "flex gap-6 items-center justify-center",
+        direction === "col" && "flex-col"
+      )}
+    >
       {/* TODO: link to actual sites */}
-      <BsInstagram className="text-primary w-7 h-7" />
-      <BsBehance className="text-primary w-7 h-7" />
-      <BsDribbble className="text-primary w-7 h-7" />
+      <BsInstagram
+        className={clsx("text-primary w-7 h-7", fontColorClasses[color])}
+      />
+      <BsBehance
+        className={clsx("text-primary w-7 h-7", fontColorClasses[color])}
+      />
+      <BsDribbble
+        className={clsx("text-primary w-7 h-7", fontColorClasses[color])}
+      />
     </div>
   );
 };
