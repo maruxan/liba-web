916b7b9b3921e5b6958bcc7ef984f4f7e957d7a2
maruxan <garavagliamauricio@gmail.com>
2023-06-19 22:11:47 -0300

    feat: social link icon size

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -1,30 +1,47 @@
 import React from "react";
 import clsx from "clsx";
-import { Color, fontColorClasses } from "@/types";
 import { BsBehance, BsDribbble, BsInstagram } from "react-icons/bs";
+import { Color, fontColorClasses } from "@/types";
 
 type Props = {
   direction?: "col" | "row";
   color?: Color;
+  size?: "md" | "lg";
 };
 
-const SocialLinks = ({ direction = "col", color = "primary" }: Props) => {
+const SocialLinks = ({
+  direction = "col",
+  color = "primary",
+  size = "md",
+}: Props) => {
   return (
     <div
       className={clsx(
-        "flex gap-6 items-center justify-center",
-        direction === "col" && "flex-col"
+        "flex gap-6 items-center justify-start",
+        direction === "col" && "flex-col justify-center"
       )}
     >
       {/* TODO: link to actual sites */}
       <BsInstagram
-        className={clsx("text-primary w-7 h-7", fontColorClasses[color])}
+        className={clsx(
+          "w-7 h-7",
+          size === "lg" && "w-10 h-10",
+          fontColorClasses[color]
+        )}
       />
       <BsBehance
-        className={clsx("text-primary w-7 h-7", fontColorClasses[color])}
+        className={clsx(
+          "w-7 h-7",
+          size === "lg" && "w-10 h-10",
+          fontColorClasses[color]
+        )}
       />
       <BsDribbble
-        className={clsx("text-primary w-7 h-7", fontColorClasses[color])}
+        className={clsx(
+          "w-7 h-7",
+          size === "lg" && "w-10 h-10",
+          fontColorClasses[color]
+        )}
       />
     </div>
   );
