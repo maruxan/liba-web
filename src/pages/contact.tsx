83dee25ed76fc64a9de03c0fff51657532399d44
maruxan <garavagliamauricio@gmail.com>
2023-06-19 22:11:58 -0300

    feat: contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,13 @@
 import Link from "next/link";
-import { Arrow, EmailInput, PageTitle, Subtitle } from "@/components";
+import Image from "next/image";
+import {
+  Arrow,
+  EmailInput,
+  PageTitle,
+  SocialLinks,
+  Subtitle,
+} from "@/components";
+import mapImage from "public/map.png";
 
 export default function Contact() {
   return (
@@ -8,17 +16,48 @@ export default function Contact() {
         <Link href="/">
           <Arrow color="white" direction="left" />
         </Link>
-        <PageTitle>Contact</PageTitle>
+        <PageTitle>
+          <span className="text-white">Contact</span>
+        </PageTitle>
       </div>
 
-      <div className="flex flex-col items-center gap-2">
-        <Subtitle>Have a chat with us.</Subtitle>
+      <div className="flex flex-col items-center gap-2 mb-14">
+        <Subtitle>
+          <span className="text-white">Have a chat with us.</span>
+        </Subtitle>
         <p className="font-bold text-2xl text-[#C4C4C4]">
           Enter your email below and we’ll get in contact
         </p>
       </div>
 
-      <EmailInput />
+      <div className="flex justify-center mb-20">
+        <EmailInput />
+      </div>
+
+      <div className="flex gap-10 flex-col items-center">
+        <Subtitle>
+          <span className="text-white">Or you can reach out in person.</span>
+        </Subtitle>
+        <div className="flex items-center justify-between gap-32">
+          <ul className="font-bold text-white space-y-3 text-2xl">
+            <li>estudioliba</li>
+            <li>8575 Nowlin Rd, AL</li>
+            <li>(262) 555-0131</li>
+            <li>
+              <SocialLinks color="white" size="lg" direction="row" />
+            </li>
+          </ul>
+          <div>
+            <Image
+              src={mapImage}
+              placeholder="blur"
+              alt="studio location on map"
+              width={350}
+              height={350}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
