71c5cb2b72baf61c2347f7b9c3c413194fa96ad4
maruxan <garavagliamauricio@gmail.com>
2023-10-05 15:39:58 -0300

    feat: contact page animations

--- src/components/email-input.tsx
+++ src/components/email-input.tsx
@@ -1,10 +1,19 @@
-import React, { FormEvent } from "react";
-import { CgArrowLongRight } from "react-icons/cg";
+import React, { FormEvent } from 'react';
+import { CgArrowLongRight } from 'react-icons/cg';
+import { motion } from 'framer-motion';
+
+const submitButtonColors = [
+  '#FFCB00',
+  '#00FFA2',
+  '#B000FF',
+  '#FF00A2',
+  '#00FFFF',
+];
 
 const EmailInput = () => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("FORM SUBMIT :::: ", e);
+    console.log('FORM SUBMIT :::: ', e);
   };
 
   return (
@@ -12,8 +21,7 @@ const EmailInput = () => {
       <div className="join rounded-none border-2 border-white w-full md:w-fit">
         <label
           htmlFor="email"
-          className="label join-item uppercase px-8 bg-white text-black font-bold tracking-wide"
-        >
+          className="label join-item uppercase px-8 bg-white text-black font-bold tracking-wide">
           email
         </label>
         <input
@@ -23,12 +31,18 @@ const EmailInput = () => {
           placeholder="example@email.com"
         />
       </div>
-      <button type="submit" className="flex items-center self-end gap-3">
-        <div className="font-bold text-2xl text-[#F9D846] pb-2">
-          catch me up
-        </div>
-        <CgArrowLongRight className="w-14 h-14" color="#F9D846" />
-      </button>
+      <motion.button
+        initial={{ color: submitButtonColors[0] }}
+        whileHover={{
+          color: submitButtonColors,
+          transition: { repeat: Infinity, duration: 1 },
+        }}
+        whileTap={{ x: 10 }}
+        type="submit"
+        className="flex items-center self-end gap-3">
+        <div className="font-bold text-2xl  pb-2">catch me up</div>
+        <CgArrowLongRight className="w-14 h-14" />
+      </motion.button>
     </form>
   );
 };
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,40 +1,49 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 import {
   Arrow,
   EmailInput,
   PageTitle,
   SocialLinks,
   Subtitle,
-} from "@/components";
-import mapImage from "public/map.png";
+} from '@/components';
+import mapImage from 'public/map.png';
 
 export default function Contact() {
   return (
     <div className="px-[8%] py-10 bg-black">
       <div className="flex justify-between items-center gap-10 mb-10 md:mb-32">
         <Link href="/">
-          <Arrow color="white" direction="left" />
+          <motion.div whileHover={{ x: -10 }}>
+            <Arrow color="white" direction="left" />
+          </motion.div>
         </Link>
         <PageTitle>
           <span className="text-white">Contact</span>
         </PageTitle>
       </div>
 
-      <div className="flex flex-col items-center gap-2 mb-14">
+      <motion.div
+        animate={{ y: [50, 0], opacity: [0, 1] }}
+        className="flex flex-col items-center gap-2 mb-14">
         <Subtitle>
           <span className="text-white">Have a chat with us.</span>
         </Subtitle>
         <p className="font-bold text-2xl text-[#C4C4C4]">
           Enter your email below and we’ll get in contact
         </p>
-      </div>
+      </motion.div>
 
-      <div className="flex justify-center mb-20">
+      <motion.div
+        animate={{ y: [50, 0], opacity: [0, 1] }}
+        className="flex justify-center mb-20">
         <EmailInput />
-      </div>
+      </motion.div>
 
-      <div className="flex gap-10 flex-col items-center">
+      <motion.div
+        animate={{ y: [50, 0], opacity: [0, 1], transition: { delay: 0.3 } }}
+        className="flex gap-10 flex-col items-center">
         <Subtitle>
           <span className="text-white">Or you can reach out in person.</span>
         </Subtitle>
@@ -57,7 +66,7 @@ export default function Contact() {
             />
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
