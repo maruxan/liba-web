04dfb7b7c0458233fbdc1ac10f6399234bfb5766
maruxan <garavagliamauricio@gmail.com>
2023-10-05 16:16:06 -0300

    feat: about page animations

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
-import Image from "next/image";
-import { Arrow, PageTitle, Subtitle, TeamCard } from "@/components";
-import brandsImg from "public/brands.png";
-import devImg from "public/development-team-member.jpg";
-import designerImg from "public/design-team-member.jpg";
-import photographerImg from "public/photography-team-member.jpg";
+import Link from 'next/link';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { Arrow, PageTitle, Subtitle, TeamCard } from '@/components';
+import brandsImg from 'public/brands.png';
+import devImg from 'public/development-team-member.jpg';
+import designerImg from 'public/design-team-member.jpg';
+import photographerImg from 'public/photography-team-member.jpg';
 
 export default function About() {
   return (
@@ -14,37 +15,65 @@ export default function About() {
           <span className="text-black">About Us</span>
         </PageTitle>
         <Link href="/">
-          <Arrow color="black" direction="right" />
+          <motion.div whileHover={{ x: 10 }}>
+            <Arrow color="black" direction="right" />
+          </motion.div>
         </Link>
       </div>
 
       <Subtitle>
-        <span className="text-black">
+        <motion.span
+          animate={{ opacity: [0, 1], transition: { delay: 0.3 } }}
+          className="text-black">
           Semper ultrices dui quis faucibus egestas sociis. Sit sapien, nulla
           tellus elit leo ac tempus donec. Tristique diam iaculis duis mauris
           integer placerat aliquet vestibulum.
-        </span>
+        </motion.span>
       </Subtitle>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 mt-10 mb-16 max-w-6xl mx-auto">
-        <TeamCard name="Greg Webb" role="design" imageSrc={designerImg} />
-        <TeamCard name="Philip Hawkins" role="development" imageSrc={devImg} />
-        <TeamCard
-          name="Gloria Alexander"
-          role="photography"
-          imageSrc={photographerImg}
-        />
+        <motion.div
+          animate={{
+            y: [100, 0],
+            opacity: [0, 1],
+            transition: { delay: 0.3 },
+          }}>
+          <TeamCard name="Greg Webb" role="design" imageSrc={designerImg} />
+        </motion.div>
+        <motion.div
+          animate={{
+            y: [100, 0],
+            opacity: [0, 1],
+            transition: { delay: 0.6 },
+          }}>
+          <TeamCard
+            name="Philip Hawkins"
+            role="development"
+            imageSrc={devImg}
+          />
+        </motion.div>
+        <motion.div
+          animate={{
+            y: [100, 0],
+            opacity: [0, 1],
+            transition: { delay: 0.9 },
+          }}>
+          <TeamCard
+            name="Gloria Alexander"
+            role="photography"
+            imageSrc={photographerImg}
+          />
+        </motion.div>
       </div>
 
-      <div className="flex justify-center">
+      <motion.div
+        animate={{ y: [100, 0], opacity: [0, 1], transition: { delay: 1.2 } }}
+        className="flex flex-col items-center gap-16 mt-32 mb-16">
         <Subtitle>
           <span className="text-black">Trusted by the best</span>
         </Subtitle>
-      </div>
-
-      <div className="flex justify-center my-16">
         <Image src={brandsImg} alt="Team member photo" />
-      </div>
+      </motion.div>
     </div>
   );
 }
